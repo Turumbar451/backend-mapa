75340f5b4cc33461037bef86d845bc686c6d6302
Emir Tirso <turumbar451@hotmail.com>
2025-10-10 20:32:34 -0600

    errores corregidos

--- models/usuario.js
+++ models/usuario.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const UsuarioSchema = new mongoose.Schema(
+const usuarioSchema = new mongoose.Schema(
     {
         username: { type: String, required: true, unique: true },
         password: { type: String, required: true },
